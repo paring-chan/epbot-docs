2b433453dc1a91fe6d8ea4166dc7b20156c463d9
pikokr <parangee9706@gmail.com>
2021-02-05 11:22:54 +0900

    feat: initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    title: '이프봇',
+    themeConfig: {
+        nav: [
+            { text: '이프 초대하기', link: 'https://discord.com/api/oauth2/authorize?client_id=693818502657867878&permissions=126016&scope=bot' },
+            { text: '키뮤의 과학실 디스코드', link: 'https://discord.gg/AmuH495BaA' },
+        ]
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,1 @@
+# EPBOT DOCS
