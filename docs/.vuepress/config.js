94b8a6f5fb41dfa63c614c277bcf87a34f9f2706
pikokr <parangee9706@gmail.com>
2021-02-05 12:05:31 +0900

    feat: backup

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+
 module.exports = {
     title: '이프봇',
     description: '낚시 & 검열 봇!',
     themeConfig: {
         nav: [
-            { text: '이프 초대하기', link: 'https://discord.com/api/oauth2/authorize?client_id=693818502657867878&permissions=126016&scope=bot' },
-            { text: '키뮤의 과학실 디스코드', link: 'https://discord.gg/AmuH495BaA' },
+            {
+                text: '이프 초대하기',
+                link: 'https://discord.com/api/oauth2/authorize?client_id=693818502657867878&permissions=126016&scope=bot'
+            },
+            {text: '키뮤의 과학실 디스코드', link: 'https://discord.gg/AmuH495BaA'},
         ],
         sidebar: [
             {
-                title: '가이드',
-                collapsable: false,
+                title: '기본',
+                path: '/guide',
                 children: [
-                    ['/guide/', '홈'],
-                    ['/guide/fish', '낚시'],
+                    '/guide/',
+                    '/guide/fish'
                 ]
             }
         ]
     },
     plugins: {
-        'vuepress-plugin-smooth-scroll': true
+        'vuepress-plugin-smooth-scroll': true,
     }
 }
