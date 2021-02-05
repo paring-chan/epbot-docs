dc5c9b7b0f84422e8797bd9fe2e27df83b6dbcfd
pikokr <parangee9706@gmail.com>
2021-02-05 11:30:21 +0900

    feat: home page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     title: '이프봇',
+    description: '낚시 & 검열 봇!',
     themeConfig: {
         nav: [
             { text: '이프 초대하기', link: 'https://discord.com/api/oauth2/authorize?client_id=693818502657867878&permissions=126016&scope=bot' },
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,7 @@
-# EPBOT DOCS
+---
+home: true
+heroImage: https://media.discordapp.net/attachments/786860960240762890/803359745137967144/c5f2c2867025c6c3.png?width=960&height=377
+actionText: 도움말 →
+actionLink: /guide/
+---
+'# Hello VuePress'
